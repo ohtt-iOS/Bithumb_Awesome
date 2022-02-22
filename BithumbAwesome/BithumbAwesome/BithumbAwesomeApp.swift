b3f8bfc83bd4a095676e925bed80532ead748ec7
소영 <sso_0022@naver.com>
2022-02-22 23:31:36 +0900

    initial commit

---
+++ BithumbAwesome/BithumbAwesome/BithumbAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BithumbAwesomeApp.swift
+//  BithumbAwesome
+//
+//  Created by soyounglee on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BithumbAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
