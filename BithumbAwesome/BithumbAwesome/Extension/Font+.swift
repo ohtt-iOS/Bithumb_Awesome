f27424199ed526e296bbb391c23d46f0d0c35be1
kangkyung <rudgns0508@naver.com>
2022-02-25 00:29:32 +0900

    add: 폰트셋 추가

---
+++ BithumbAwesome/BithumbAwesome/Extension/Font+.swift
@@ -0,0 +1,19 @@
+//
+//  Font+.swift
+//  BithumbAwesome
+//
+//  Created by 강경 on 2022/02/24.
+//
+
+import SwiftUI
+
+extension Font {
+  static let heading1: Font = .system(size: 36, weight: .bold)
+  static let heading2: Font = .system(size: 20, weight: .bold)
+  static let heading3: Font = .system(size: 16, weight: .semibold)
+  static let heading4: Font = .system(size: 14, weight: .semibold)
+  static let heading5: Font = .system(size: 14, weight: .regular)
+  static let heading6: Font = .system(size: 12, weight: .regular)
+  static let heading7: Font = .system(size: 10, weight: .regular)
+  static let heading8: Font = .system(size: 8, weight: .regular)
+}
