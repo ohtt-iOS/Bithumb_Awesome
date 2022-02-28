f6732735b932bed27f751be943cce30d46ec6c6f
소영 <sso_0022@naver.com>
2022-03-01 00:50:56 +0900

    feat: RadioButtonView 구현

--- BithumbAwesome/BithumbAwesome/Custom/AwesomeButton/RadioButtonView.swift
+++ BithumbAwesome/BithumbAwesome/Custom/AwesomeButton/RadioButtonView.swift
@@ -5,4 +5,52 @@
 //  Created by ohtt on 2022/02/28.
 //
 
-import Foundation
+import SwiftUI
+import ComposableArchitecture
+
+struct RadioButtonState: Equatable {
+  var buttons: [AwesomeButtonType]
+  var selectedButton: AwesomeButtonType
+}
+
+enum RadioButtonAction: Equatable {
+  case buttonTap(AwesomeButtonType)
+}
+
+struct RadioButtonEnvironment { }
+
+let radioButtonReducer = Reducer<RadioButtonState, RadioButtonAction, RadioButtonEnvironment> { state, action, environment in
+  switch action {
+  case let .buttonTap(type):
+    state.selectedButton = type
+    return .none
+  }
+}
+
+struct RadioButtonView: View {
+  var store: Store<RadioButtonState, RadioButtonAction>
+  
+  var body: some View {
+    WithViewStore(self.store) { viewStore in
+      HStack(spacing: 15) {
+        ForEach(viewStore.state.buttons) { button in
+          let selectedState = button == viewStore.state.selectedButton
+          Button(
+            action: { viewStore.send(.buttonTap(button)) },
+            label: {
+              HStack(spacing: 5) {
+                button.iconImage
+                  .foregroundColor(selectedState ? Color.aGray1 : Color.aGray3)
+                Text(button.textString)
+              }
+              .padding(.horizontal, 10)
+            }
+          )
+            .buttonStyle(AwesomeButtonStyle(isSelected: selectedState))
+        }
+      }
+    }
+  }
+}
+
+
