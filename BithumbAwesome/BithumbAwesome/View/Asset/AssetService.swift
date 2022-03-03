23fca7f3989b94d5d63aed6f5e77290e918255e4
kangkyung <rudgns0508@naver.com>
2022-03-03 18:29:43 +0900

    feat: AssetView - Service에 API 연결
    [#20]

--- BithumbAwesome/BithumbAwesome/View/Asset/AssetService.swift
+++ BithumbAwesome/BithumbAwesome/View/Asset/AssetService.swift
@@ -10,7 +10,7 @@ import ComposableArchitecture
 import Alamofire
 
 struct AssetService {
-  var fetchAssetData: () -> Effect<[Any], Failure>
+  var fetchAssetData: () -> Effect<[Asset], Failure>
   struct Failure: Error, Equatable {}
 }
 
@@ -18,7 +18,41 @@ extension AssetService {
   static let asset = AssetService(
     fetchAssetData: {
       Effect.run { subscriber in
-        subscriber.send(completion: .failure(Failure()))
+        let URL = "https://api.bithumb.com/public/assetsstatus/ALL"
+        let headers: HTTPHeaders = [
+          "Content-Type": "application/json"
+        ]
+        let dataRequest = AF.request(
+          URL,
+          method: .get,
+          encoding: JSONEncoding.default,
+          headers: headers
+        )
+        
+        // TODO: 디코딩 로직개선
+        dataRequest
+          .validate(statusCode: 200..<300)
+          .responseData { response in
+            switch response.result {
+            case .success(let value):
+              do {
+                if let json = try JSONSerialization.jsonObject(with: value, options: []) as? [String: Any] {
+                  var items = json["data"] as? [String: Any]
+                  let result = try items?.map { key, value -> Asset in
+                    let valueData = try JSONSerialization.data(withJSONObject: value, options: [])
+                    let assetResponse = try JSONDecoder().decode(AssetData.self, from: valueData)
+                    return Asset(ticker: key, data: assetResponse)
+                  }
+                  subscriber.send(result!)
+                }
+              } catch {
+                subscriber.send(completion: .failure(Failure()))
+              }
+            case .failure(_):
+              subscriber.send(completion: .failure(Failure()))
+              break
+            }
+          }
         return AnyCancellable {}
       }
     }
