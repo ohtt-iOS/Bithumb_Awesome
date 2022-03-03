f58e1cbe0d9f85957d8afc557fff337c06a88545
소영 <sso_0022@naver.com>
2022-03-04 01:27:35 +0900

    feat: ResponseResult 추가

---
+++ BithumbAwesome/BithumbAwesome/Model/ResponseResult.swift
@@ -0,0 +1,13 @@
+//
+//  ResponseResult.swift
+//  BithumbAwesome
+//
+//  Created by ohtt on 2022/03/04.
+//
+
+import Foundation
+
+struct ResponseResult<T: Codable>: Codable {
+  var status: String?
+  var data: [T]?
+}
