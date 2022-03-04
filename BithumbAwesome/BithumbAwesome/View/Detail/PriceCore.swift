a51fa37d7737674144b1e14efff66fa18b50f8f0
소영 <sso_0022@naver.com>
2022-03-04 14:56:01 +0900

    feat: PriceCore 추가 (#6)

---
+++ BithumbAwesome/BithumbAwesome/View/Detail/PriceCore.swift
@@ -0,0 +1,22 @@
+//
+//  PriceCore.swift
+//  BithumbAwesome
+//
+//  Created by ohtt on 2022/03/04.
+//
+
+import ComposableArchitecture
+
+struct PriceState: Equatable {
+}
+
+enum PriceAction: Equatable {
+}
+
+struct PriceEnvironment {
+}
+
+let priceReducer = Reducer<PriceState, PriceAction, PriceEnvironment> { state, action, environment in
+    switch action {
+  }
+}
