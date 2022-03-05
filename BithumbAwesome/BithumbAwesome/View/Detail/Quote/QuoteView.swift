537d95a5d5aa430d70b4c82a2a2234f818c2e8bf
kangkyung <rudgns0508@naver.com>
2022-03-05 14:52:48 +0900

    feat: QuoteView - 스크롤뷰 가운데부터 나오도록 세팅
    [#10]

--- BithumbAwesome/BithumbAwesome/View/Detail/Quote/QuoteView.swift
+++ BithumbAwesome/BithumbAwesome/View/Detail/Quote/QuoteView.swift
@@ -11,9 +11,23 @@ import SwiftUI
 struct QuoteView: View {
   let store: Store<QuoteState, QuoteAction>
   
+  private let scrollViewIdentifier: Int = 1
+  
   var body: some View {
     WithViewStore(self.store) { viewStore in
-      Text("Quote")
+      ScrollViewReader { scrollViewProxy in
+        ScrollView(showsIndicators: false) {
+          VStack(spacing: 1) {
+            ForEach(0..<100) { index in
+              Text("QuoteListRow\(index)")
+            }
+          }
+          .id(self.scrollViewIdentifier)
+        }
+        .onAppear {
+          scrollViewProxy.scrollTo(self.scrollViewIdentifier, anchor: .center)
+        }
+      }
     }
   }
 }
