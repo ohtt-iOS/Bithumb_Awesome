d7b05b07f5890b33abfb9ed5f5d6fddc5e68da2e
kangkyung <rudgns0508@naver.com>
2022-03-05 18:03:58 +0900

    feat: QuoteView - 부가정보 리스트뷰 세팅
    [#10]

--- BithumbAwesome/BithumbAwesome/View/Detail/Quote/List/QuoteAdditionalInfomationList.swift
+++ BithumbAwesome/BithumbAwesome/View/Detail/Quote/List/QuoteAdditionalInfomationList.swift
@@ -8,11 +8,106 @@
 import SwiftUI
 
 struct QuoteAdditionalInfomationList: View {
+  private let rowHorizontalPadding: CGFloat = 1
+  private let rowSpacing: CGFloat = 10
+  
   var body: some View {
-    VStack {
+    VStack(spacing: self.rowSpacing) {
       Spacer()
       
-      Text("QuoteAdditionalInfomationList")
+      ForEach(ListRowType.allCases) { listRowType in
+        ListRow(type: listRowType)
+
+        if listRowType == .transactionAmount {
+          HorizonDivider()
+        }
+      }
+    }
+    .padding(.horizontal, self.rowHorizontalPadding)
+  }
+}
+
+private struct ListRow: View {
+  let type: ListRowType
+  
+  private let rowSpacing: CGFloat = 5
+  
+  var body: some View {
+    VStack(spacing: self.rowSpacing) {
+      HStack {
+        Text(self.type.rowTitleString)
+          .font(Font.heading7)
+          .foregroundColor(Color.aGray2)
+        
+        Spacer()
+        
+        Text(self.type.rowValueString)
+          .font(Font.heading7)
+          .foregroundColor(
+            self.type == .highPrice ? Color.aRed1 :
+              (self.type == .lowPrice ? Color.aBlue1 : Color.aGray2)
+          )
+      }
+      
+      HStack {
+        Spacer()
+        
+        if self.type == .highPrice {
+          Text("0.18%")
+            .font(Font.heading7)
+            .foregroundColor(Color.aRed1)
+        } else if self.type == .lowPrice {
+          Text("-4.8%")
+            .font(Font.heading7)
+            .foregroundColor(Color.aBlue1)
+        }
+      }
+    }
+  }
+}
+
+private enum ListRowType {
+  case transactionVolume, transactionAmount
+  case closingPrice, marketPrice, highPrice, lowPrice
+}
+
+extension ListRowType: CaseIterable, Identifiable {
+  var id: UUID {
+    UUID()
+  }
+}
+
+extension ListRowType {
+  var rowTitleString: String {
+    switch self {
+    case .transactionVolume:
+      return "거래량"
+    case .transactionAmount:
+      return "거래금"
+    case .closingPrice:
+      return "전일종가"
+    case .marketPrice:
+      return "시가(당일)"
+    case .highPrice:
+      return "고가(당일)"
+    case .lowPrice:
+      return "저가(당일)"
+    }
+  }
+  var rowValueString: String {
+    switch self {
+    case .transactionVolume:
+      return "3.165.686 BTC"
+    case .transactionAmount:
+      return "1,603,900 억"
+    case .closingPrice:
+      return "52,989,000"
+    case .marketPrice:
+      return "52,989,000"
+    case .highPrice:
+      return "53,087,000"
+    case .lowPrice:
+      return "50,443,000"
     }
   }
 }
